354bd12e535ea10f029780ab38fff4f1d1c25306
Michael Davis <mcarsondavis@gmail.com>
2025-02-25 11:20:43 -0500

    locals: Fix comparison conditions for creating the scope cursor
    I updated this in be5c489 but didn't get the conditions quite right. We
    want to skip over any children scopes that end before the given starting
    position, so we should compare that the range end is less than the
    position. The locals prototype in fabc0f0 was closer: it just needed the
    sides of the comparison swapped.

--- highlighter/src/locals.rs
+++ highlighter/src/locals.rs
@@ -73,12 +73,12 @@ impl Locals {
             let scope_data = &self[scope];
             let child_idx = scope_data
                 .children
-                .partition_point(|&child| self[child].range.start < pos);
+                .partition_point(|&child| self[child].range.end < pos);
             scope_stack.push((scope, child_idx as u32));
             let Some(&child) = scope_data.children.get(child_idx) else {
                 break;
             };
-            if pos <= self[child].range.start {
+            if pos < self[child].range.start {
                 break;
             }
             scope = child;
@@ -227,5 +227,52 @@ impl Syntax {
         }
 
         layer_data.locals = locals;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn cursor() {
+        let mut locals = Locals::default();
+        let scope1 = locals.push(ScopeData {
+            definitions: Default::default(),
+            range: 5..105,
+            inherit: true,
+            // NOTE: the subsequent call to `push` below will add scope2 to scope1's children.
+            children: Default::default(),
+            parent: Some(Scope::ROOT),
+        });
+        let scope2 = locals.push(ScopeData {
+            definitions: Default::default(),
+            range: 10..100,
+            inherit: true,
+            children: Default::default(),
+            parent: Some(scope1),
+        });
+
+        let mut cursor = locals.scope_cursor(0);
+        assert_eq!(cursor.current_scope(), Scope::ROOT);
+        assert_eq!(cursor.advance(3), Scope::ROOT);
+        assert_eq!(cursor.advance(5), scope1);
+        assert_eq!(cursor.advance(8), scope1);
+        assert_eq!(cursor.advance(10), scope2);
+        assert_eq!(cursor.advance(50), scope2);
+        assert_eq!(cursor.advance(100), scope1);
+        assert_eq!(cursor.advance(105), Scope::ROOT);
+        assert_eq!(cursor.advance(110), Scope::ROOT);
+
+        let mut cursor = locals.scope_cursor(8);
+        assert_eq!(cursor.current_scope(), scope1);
+        assert_eq!(cursor.advance(10), scope2);
+        assert_eq!(cursor.advance(100), scope1);
+        assert_eq!(cursor.advance(110), Scope::ROOT);
+
+        let mut cursor = locals.scope_cursor(10);
+        assert_eq!(cursor.current_scope(), scope2);
+        assert_eq!(cursor.advance(100), scope1);
+        assert_eq!(cursor.advance(110), Scope::ROOT);
     }
 }
