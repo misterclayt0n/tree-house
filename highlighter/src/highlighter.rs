1fa65eca36fdbb2837e0655bfda53ed627fc25c0
Michael Davis <mcarsondavis@gmail.com>
2025-03-23 16:11:40 -0400

    highlighter: Publicize Highlight::get

--- highlighter/src/highlighter.rs
+++ highlighter/src/highlighter.rs
@@ -130,7 +130,7 @@ impl Highlight {
         Self(unsafe { NonZeroU32::new_unchecked(inner ^ u32::MAX) })
     }
 
-    const fn get(&self) -> u32 {
+    pub const fn get(&self) -> u32 {
         self.0.get() ^ u32::MAX
     }
 
